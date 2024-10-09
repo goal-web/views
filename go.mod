762a9bf5c81ae2b536f46d484977bdaf0ac253d0
qbhy <96qbhy@gmail.com>
2024-10-09 16:14:51 +0400

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/goal-web/views
+
+go 1.23.2
---
+++ service_provider.go
@@ -0,0 +1,23 @@
+package views
+
+import (
+	"github.com/goal-web/contracts"
+)
+
+type ServiceProvider struct {
+}
+
+func (s ServiceProvider) Register(app contracts.Application) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s ServiceProvider) Start() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s ServiceProvider) Stop() {
+	//TODO implement me
+	panic("implement me")
+}
